cdf5b39e14d7aff228b2387459e89131a052b60c
Joao Fuzer <fuuzer_@hotmail.com>
2026-03-24 18:29:55 -0300

    add: imports header footer and react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import './App.css'
 import Api from "./api/Api"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./Components/Home"
 
 function App() {
 
   return (
     <>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
