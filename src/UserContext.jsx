5b56301a3e3ee3158137cad1ee8b2595588e94c5
Joao Fuzer <fuuzer_@hotmail.com>
2026-04-01 19:43:50 -0300

    add userContext

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { TOKEN_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const resp = await fetch(url, options);
+    const json = await resp.json()
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password });
+    const tokenResp = await fetch(url, options);
+    const { token } = await tokenResp.json();
+    window.localStorage.setItem('token', token);
+    getUser(token);
+  }
+
   return (
-    <UserContext.Provider>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ userLogin, data }}>
+      {children}
+    </UserContext.Provider>
   )
 }
 
