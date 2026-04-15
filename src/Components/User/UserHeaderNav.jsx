9dc0bb976c512df0adf71b8e2ede1d61f42c5c49
Joao Fuzer <fuuzer_@hotmail.com>
2026-04-14 21:16:14 -0300

    started UserHeaderNav styles

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router'
+import { UserContext } from '../../UserContext'
+import styles from './UserHeaderNav.module.css'
+import MinhasFotos from '../../assets/feed.svg?react'
+import Estatisticas from '../../assets/estatisticas.svg?react'
+import AdicionarFoto from '../../assets/adicionar.svg?react'
+import Sair from '../../assets/sair.svg?react'
+
 
 const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext)
   return (
-    <nav>
-      <NavLink to='/conta'>Minhas Fotos</NavLink>
-      <NavLink to='/conta/estatisticas'>Estatísticas</NavLink>
-      <NavLink to='/conta/postar'>Adicionar foto</NavLink>
-      <button>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to='/conta'><MinhasFotos />Minhas fotos</NavLink>
+      <NavLink to='/conta/estatisticas'><Estatisticas />Estatisticas</NavLink>
+      <NavLink to='/conta/postar'><AdicionarFoto />Adicionar Foto</NavLink>
+      <button onClick={userLogout}><Sair />Sair</button>
     </nav>
   )
 }
